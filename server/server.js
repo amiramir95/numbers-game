e08fe0d8c86ce0a6aab5fbae229746111a11ca1c
Amir Aloui <amiraloui20@gmail.com>
2023-11-24 16:07:42 +0100

    add env

--- server/server.js
+++ server/server.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5001
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/highscore', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING)
 
 // Create a Mongoose model for high scores
 const HighScore = mongoose.model('HighScore', {
   name: String,
   score: Number,
-});
+})
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/highscore', async (req, res) => {
   try {
-    const highestScore = await HighScore.findOne().sort({ score: -1 });
-    res.json(highestScore);
+    const highestScore = await HighScore.findOne().sort({ score: -1 })
+    res.json(highestScore)
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: 'Internal Server Error' })
   }
-});
+})
 
 app.post('/highscore', async (req, res) => {
-  const { name, score } = req.body;
+  const { name, score } = req.body
 
   try {
-    const newHighScore = new HighScore({ name, score });
-    await newHighScore.save();
-    res.json({ success: true });
+    const newHighScore = new HighScore({ name, score })
+    await newHighScore.save()
+    res.json({ success: true })
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: 'Internal Server Error' })
   }
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
